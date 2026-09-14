b44044115fe28575cc95f8cc471b48bdf85e239d
antonypaul98 <55086566+antonypaul98@users.noreply.github.com>
2026-09-13 21:25:16 -0400

    feat(career): add Career Agent domain package

---
+++ app/career/__init__.py
@@ -0,0 +1,17 @@
+"""Career Agent domain package."""
+
+from .models import (
+    CareerFact,
+    FactSensitivity,
+    FactSource,
+    MasterCareerProfile,
+    RoleVariant,
+)
+
+__all__ = [
+    "CareerFact",
+    "FactSensitivity",
+    "FactSource",
+    "MasterCareerProfile",
+    "RoleVariant",
+]
